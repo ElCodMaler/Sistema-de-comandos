c58b58ed07f14b54112f3f7982b3c844c6b5d718
ElCodMaler <140354428+ElCodMaler@users.noreply.github.com>
2024-01-30 03:36:01 -0400

    informacion agregada

--- Terminal.py
+++ Terminal.py
@@ -1,17 +1,26 @@
-from elementos_terminal import Fichero, Carpeta, Unidad
-from Comandos import Comando
+#from elementos_terminal import Fichero, Carpeta, Unidad
+#from Comandos import Comando
+import json
 #clase
-class Consola(Fichero, Carpeta, Unidad, Comando):
-    def crearCarpeta(self):
-        self.carpetas = []
-        self.carpetas.append(Fichero(1,'foto sonriendo','.jpg',2500,''))
-        self.carpetas.append(Fichero(2,'video de la playa','.mp4',23865,''))
-        self.carpetas.append(Fichero(3,'notas de la universidad','.txt',958,'Estructuras del cuerpo y la ciencia que explica su contenido'))
-        self.carpetas.append(Fichero(4,'estadisticas de la tasa de valores','.exe',1200,'22% de ganancias en el mercado negro'))
-        self.carpetas.append(Fichero(5,'foto de mi gato','.jpg',1200,''))
-        self.carpetas.append(Fichero(6,'video de un baile','.mp4',5300,''))
-    def crearUnidad(self,)
- 
+# class informacion(Fichero, Carpeta, Unidad, Comando):
+#     def crearCarpeta(self):
+#         self.carpetas = []
+#         self.carpetas.append()
+#     def crearUnidad(self,)
+#obteniendo datos del archivo Json
+with open("datos.json","r") as info:
+    datos = info.read()
+#decodificando el archivo Json a un diccionario
+dict_datos = json.JSONDecoder().decode(datos)
+#
+for i,j in dict_datos.items():
+    print(f'{i}')
+
+    for m, n in j.items():
+        print(m)
+
+        # for l, p in n.items():
+        #     print(l)
 
 
         
